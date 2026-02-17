504e5ef8a1e4a51a02351ce1d30af57166e3413e
SamuelMarugan <adm.pc.samu9920@gmail.com>
2026-02-17 12:14:05 +0100

    session 2

--- app/models.py
+++ app/models.py
@@ -28,3 +28,12 @@ class Pizza(models.Model):
 
     def __str__(self):
         return self.nombre
+
+class Proveedor (models.Model):
+    nombre = models.CharField(max_length=30)
+    email = models.EmailField(unique=True)
+    telefono = models.CharField(blank=True, max_length=15)
+    fecha_alta = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.nombre
